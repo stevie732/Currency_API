9f2bfb7541eb66af633621bb8c425d130c94f73d
Stevie732 <stephenprahljr@outlook.com>
2024-08-27 02:31:17 -0400

    'fixed'

--- main.js
+++ main.js
@@ -4,12 +4,13 @@ var dog = document.getElementById("dogecoin");
 
 var settings = {
   "async": true,
-  "scrossDomain": true,
-  "url": "https://pro-api.coingecko.com/api/v3/coins/list",
-"method": "GET",
-"headers": {}
+  "crossDomain": true,
+  "url": "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,dogecoin&vs_currencies=usd",
+  "method": "GET",
+  "headers": {}
 }
-$.ajax(settings).done(function (response){
+
+$.ajax(settings).done(function (response) {
   btc.innerHTML = response.bitcoin.usd;
   eth.innerHTML = response.ethereum.usd;
   dog.innerHTML = response.dogecoin.usd;
